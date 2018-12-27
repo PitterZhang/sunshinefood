2a27a6c44e01f415e8a2322a888dd194fdf24cd2
ry <2351633417@qq.com>
2018-12-27 15:56:50 +0800

    1

--- Sunshinefood/sunshine-core/src/main/java/org/dao/BuyerMapper.java
+++ Sunshinefood/sunshine-core/src/main/java/org/dao/BuyerMapper.java
@@ -1,7 +1,9 @@
 package org.dao;
 
+import org.apache.ibatis.annotations.Select;
 import org.entity.Buyer;
 
 public interface BuyerMapper {
+	@Select("SELECT * FROM buyer where buyer_name=#{buyer.buyerName} and buyer_password =#{buyer.buyerPassword}")
 	Buyer login(Buyer buyer);
 }
